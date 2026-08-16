a15193c4bb10a11fe2624be77c2d0bff5bdd01ce
jordan espinoza <111539040+Espinoza0007@users.noreply.github.com>
2026-08-16 16:56:29 -0600

    Fix MAUI 10 platform configuration

--- SDV.Next/src/SDV.Maui/Platforms/Android/MainApplication.cs
+++ SDV.Next/src/SDV.Maui/Platforms/Android/MainApplication.cs
@@ -1,9 +1,10 @@
 using Android.App;
 using Android.Runtime;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
 namespace SDV.Maui;
 [Application]
 public class MainApplication(nint handle, JniHandleOwnership ownership) : MauiApplication(handle, ownership)
 {
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
-
